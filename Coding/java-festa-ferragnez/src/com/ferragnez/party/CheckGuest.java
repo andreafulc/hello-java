2e22eccf3694f6c10697f3d0448656db7915dad2
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-05-06 16:08:11 +0200

    feat - scanner for autentication

--- Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -7,7 +7,9 @@ public class CheckGuest {
 // only the name in the list can enter the party
 String[] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"}
 
-
+// ask the name of the guest
+System.out.println("inserire nome e cognome: ");
+Scanner whoAreYou = new Scanner(System.in);
 
     }
     
