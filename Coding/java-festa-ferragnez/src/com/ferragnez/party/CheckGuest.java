e0b7e6a594dac634c9038c92d99be1576aaef66a
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-05-06 16:51:30 +0200

    feat - bonus doesn't work well

--- Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -18,17 +18,43 @@ String whoAreYou = scanner.nextLine();
 scanner.close();
 
 
-// check if the name is in the list
+// check IF the name is in the list
 
- if (whoAreYou.equals(guest[0]) || whoAreYou.equals(guest[1]) || whoAreYou.equals(guest[2]) || whoAreYou.equals(guest[3]) || whoAreYou.equals(guest[4]) || whoAreYou.equals(guest[5]) || whoAreYou.equals(guest[6]) || whoAreYou.equals(guest[7]) || whoAreYou.equals(guest[8]) || whoAreYou.equals(guest[9])) {
-     System.out.println("Benvenuto alla festa!");
- } else {
-     System.out.println("Mi dispiace, non sei nella lista degli invitati.");
+//  if (whoAreYou.equals(guest[0]) || whoAreYou.equals(guest[1]) || whoAreYou.equals(guest[2]) || whoAreYou.equals(guest[3]) || whoAreYou.equals(guest[4]) || whoAreYou.equals(guest[5]) || whoAreYou.equals(guest[6]) || whoAreYou.equals(guest[7]) || whoAreYou.equals(guest[8]) || whoAreYou.equals(guest[9])) {
+//      System.out.println("Benvenuto alla festa!");
+//  } else {
+//      System.out.println("Mi dispiace, non sei nella lista degli invitati.");
     
- }
+//  }
+
+// check if the name is in the list using a for LOOP
+
+// for (int i = 0; i < guest.length; i++) {
+//    if (whoAreYou.equals(guest[i])) {
+//          System.out.println("Benvenuto alla festa!");
+//          break;
+//     } else if (i == guest.length - 1) {
+//             System.out.println("Mi dispiace, non sei nella lista degli invitati.");
+//         }
+//     }
 
 
 
+// check if the name is in the list using a while LOOP
+
+int i = 0;
+
+while (i < guest.length) {
+    if (whoAreYou.equals (guest[i]) ) {
+        System.out.println("Benvenuto alla festa!");
+        break;
+    } else {
+        System.out.println("Mi dispiace, non sei nella lista degli invitati.");
+        i++;
+    } 
+
+    
+}
 
 
     }}
