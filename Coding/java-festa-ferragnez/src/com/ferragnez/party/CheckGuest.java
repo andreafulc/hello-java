d93fe508b4d7937c491391dab99a66f3e04c2d23
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-05-06 16:20:30 +0200

    feat - scanner fixed

--- Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -1,16 +1,24 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
     public static void main(String[] args) {
         
 // only the name in the list can enter the party
-String[] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"}
+String[] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+Scanner scanner = new Scanner(System.in);
 
 // ask the name of the guest
 System.out.println("inserire nome e cognome: ");
-Scanner whoAreYou = new Scanner(System.in);
+String whoAreYou = scanner.nextLine();
 
-    }
+scanner.close();
+
+
+
+
     
 }
