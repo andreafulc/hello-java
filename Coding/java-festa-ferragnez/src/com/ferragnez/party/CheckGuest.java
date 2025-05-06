386dc357f89aecaa187a9e0cfe0e6433659416ab
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-05-06 16:06:53 +0200

    feat - array list name for guest

--- Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ Coding/java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -4,6 +4,9 @@ public class CheckGuest {
 
     public static void main(String[] args) {
         
+// only the name in the list can enter the party
+String[] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"}
+
 
 
     }
